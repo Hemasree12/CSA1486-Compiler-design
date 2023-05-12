bea2c95ca2d0b9d64af7e45dde400c114a5a175c
Hemasree12 <112635103+Hemasree12@users.noreply.github.com>
2023-05-12 08:41:58 +0530

    Update exp5 whitespaces.c

--- exp5 whitespaces.c
+++ exp5 whitespaces.c
@@ -1,35 +1,32 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
 int main()
 {
-    char str[100];
-
-    int words=0,newline=0,characters=0;
-
-    scanf("%[^~]",&str);   
-
-    for(int i=0;str[i]!='\0';i++)
-     { 
-         if(str[i] == ' ')
-         { 
-              words++;
-         }
-         else if(str[i] == '\n')
-         {
-             newline++;
-              words++;
-         }
-         else if(str[i] != ' ' && str[i] != '\n'){
-         characters++;
-         }
-     }
-    if(characters > 0)
-    {
-        words++;
-        newline++;
-    }
-     printf("Total number of words : %d\n",words);
-     printf("Total number of lines : %d\n",newline);
-     printf("Total number of characters : %d\n",characters);
-    return 0;
+   FILE *fp;
+   char ch, fname[30];
+   int noOfChar=0, noOfSpace=0, noOfTab=0, noOfNewline=0;
+   printf("Enter file name with extension: ");
+   gets(fname);
+   fp = fopen(fname, "r");
+   while(fp)
+   {
+      ch = fgetc(fp);
+      if(ch==EOF)
+         break;
+      noOfChar++;
+      if(ch==' ')
+         noOfSpace++;
+      if(ch=='\t')
+         noOfTab++;
+      if(ch=='\n')
+         noOfNewline++;
+   }
+   fclose(fp);
+   printf("\nNumber of characters = %d", noOfChar);
+   printf("\nNumber of spaces = %d", noOfSpace);
+   printf("\nNumber of tabs = %d", noOfTab);
+   printf("\nNumber of newline = %d", noOfNewline);
+   getch();
+   return 0;
 }
 
